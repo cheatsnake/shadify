cf7a1b8dd6d12ce933a72df78816ca48ebb611ff
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-06-26 14:23:18 +0300

    create server

---
+++ cmd/server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os"
+	_ "time"
+
+	"github.com/cheatsnake/shadify/internal/routes"
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/gofiber/fiber/v2/middleware/limiter"
+)
+
+func main() {
+	app := fiber.New()
+	port := os.Getenv("PORT")
+
+	if os.Getenv("PORT") == "" {
+		port = "5000"
+	}
+
+	// app.Use(limiter.New(limiter.Config{
+	// 	Expiration: 10 * time.Second,
+	// 	Max: 5,
+	// }))
+
+	routes.MathRouter(app)
+	routes.TakuzuRouter(app)
+	routes.SudokuRouter(app)
+	routes.SchulteRouter(app)
+
+	log.Fatal(app.Listen(":" + port))
+}
