903384cf59d98ab2ec0cd410e874ab4da4174dd1
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-07-06 21:00:22 +0300

    update minesweeper module

--- pkg/minesweeper/core.go
+++ pkg/minesweeper/core.go
@@ -1,11 +1,23 @@
 package minesweeper
 
+import "errors"
+
 func NewCore() *Core {
 	return &Core{}
 }
 
-func (sc *Core) Generate(sp string, w, h, mines int) Board {
-	board := generator(sp, w, h, mines)
+func (sc *Core) Generate(sp string, w, h, mines int) (Board, error) {
+
+	if mines >= (w * h) {
+		return Board{}, errors.New("the field is too small for so many mines")
+	}
+
+	startX, startY, err := parseStartPosition(sp, w, h)
+	if err != nil {
+		return Board{}, err
+	}
+
+	board := generator(startX, startY, w, h, mines)
 
 	return Board{
 		StartPosition: sp,
@@ -13,5 +25,5 @@ func (sc *Core) Generate(sp string, w, h, mines int) Board {
 		Height:        h,
 		Board:         board,
 		Mines:         mines,
-	}
+	}, nil
 }
--- pkg/minesweeper/generator.go
+++ pkg/minesweeper/generator.go
@@ -1,12 +1,14 @@
 package minesweeper
 
 import (
+	"errors"
 	"strconv"
+	"strings"
 
 	"github.com/cheatsnake/shadify/pkg/assists"
 )
 
-func generator(sp string, w, h, mines int) [][]string {
+func generator(startX, startY, w, h, mines int) [][]string {
 	board := make([][]string, h)
 
 	for i := 0; i < h; i++ {
@@ -14,9 +16,9 @@ func generator(sp string, w, h, mines int) [][]string {
 	}
 
 	for i := 0; i < mines; i++ {
-		x := assists.GetRandomInteger(0, w-1)
-		y := assists.GetRandomInteger(0, h-1)
-		if board[y][x] == "x" {
+		x := assists.GetRandomInteger(0, w - 1)
+		y := assists.GetRandomInteger(0, h - 1)
+		if board[y][x] == "x" || (x == startX && y == startY)  {
 			i--
 		} else {
 			board[y][x] = "x"
@@ -32,6 +34,22 @@ func generator(sp string, w, h, mines int) [][]string {
 	}
 
 	return board
+}
+
+func parseStartPosition(sp string, w, h int) (int, int, error) {
+	posValues := strings.Split(sp, "-")
+	if len(posValues) < 2 {
+		return 0, 0, errors.New("incorrect start position")
+	}
+
+	x, errX := strconv.Atoi(posValues[0])
+	y, errY := strconv.Atoi(posValues[1])
+
+	if errX != nil || errY != nil || x > w || y > h || x < 1 || y < 1 {
+		return 0, 0, errors.New("incorrect start position")
+	}
+
+	return x - 1, y - 1, nil
 }
 
 func lookAround(board [][]string, y, x int) string {
