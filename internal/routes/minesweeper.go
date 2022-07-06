4b30b5338b5045031efe0508e2df78eb467597aa
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-07-06 21:41:33 +0300

    add minesweeper router

---
+++ internal/routes/minesweeper.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github.com/cheatsnake/shadify/internal/handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func MinesweeperRouter(app fiber.Router) {
+	prefix := "/minesweeper"
+
+	app.Get(prefix + "/generator", handlers.MinesweeperGenerator)
+}
