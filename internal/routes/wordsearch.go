242eada37c53f554990408d9efb385f022436c85
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-07-17 08:39:25 +0300

    add wordsearch router

---
+++ internal/routes/wordsearch.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github.com/cheatsnake/shadify/internal/handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func WordsearchRouter(app fiber.Router) {
+	prefix := "/api/wordsearch"
+
+	app.Get(prefix + "/generator", handlers.WordsearchGenerator)
+}
