f7dfbe88099371159e2ff1ede3c111f5b40f1d6f
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-07-22 15:28:34 +0300

    update anagram module

--- pkg/anagrams/core.go
+++ pkg/anagrams/core.go
@@ -1,10 +1,7 @@
 package anagrams
 
 import (
-	"fmt"
-	"math/rand"
 	"path/filepath"
-	"strings"
 
 	"github.com/cheatsnake/shadify/pkg/assists"
 )
@@ -21,40 +18,10 @@ func init() {
 	wordsDBSize = len(nouns)
 }
 
-func Generate() {
+// Generate an anagram with random task and random hiden words
+func Generate() Anagram {
 	word := getRandomWord(9, 15)
 	words := composingWords(word)
 
-	fmt.Println(Anagram{Task: word, Words: words})
-}
-
-func getRandomWord(minLength, maxLength int) string {
-	var word string
-
-	for len(word) < minLength || len(word) > maxLength {
-		word = wordsDB[rand.Intn(wordsDBSize)]
-	}
-
-	return word
-}
-
-func composingWords(word string) []string {
-	result := make([]string, 0, 32)
-
-	for _, w := range wordsDB {
-		isFits := true
-		for _, letter := range w {
-			if !(strings.Count(w, string(letter)) <= strings.Count(word, string(letter))) || len(w) < 3 {
-				isFits = false
-			}
-		}
-
-		if isFits {
-			result = append(result, w)
-		}
-	}
-
-	result = assists.RemoveElement(result, assists.IndexOf(word, result))
-
-	return result
+	return Anagram{Task: word, Words: words}
 }
