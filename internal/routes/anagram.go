f04c2f7748770a71a38230dc8d4a27975ba9cfc9
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-07-23 20:20:35 +0300

    add anagram router

---
+++ internal/routes/anagram.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github.com/cheatsnake/shadify/internal/handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func AnagramRouter(app fiber.Router) {
+	prefix := "/api/anagram"
+
+	app.Get(prefix + "/generator", handlers.AnagramGenerator)
+}
