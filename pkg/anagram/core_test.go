7af2d1fb3afd81f33bc47d0f7bbeee23c6220442
Yury <72691412+cheatsnake@users.noreply.github.com>
2022-08-03 19:21:31 +0300

    test: Add tests for anagram module

---
+++ pkg/anagram/core_test.go
@@ -0,0 +1,24 @@
+package anagram
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/cheatsnake/shadify/pkg/assists"
+)
+
+func TestGenerate(t *testing.T) {
+	wordsDB, _ = assists.ReadFileLineByLine(filepath.Join("..", "..", "data", "nouns.txt"))
+	wordsDBSize = len(wordsDB)
+	result := Generate()
+
+	for i := 0; i < 10; i++ {
+		if !(len(result.Task) >= 9 && len(result.Words) > 0) {
+			t.Errorf(
+				"Generate() FAILED. Expected result.Task with length >= 9, but got result.Task length = %d and result.Words length = %d.",
+				len(result.Task), len(result.Words))
+		} else {
+			t.Logf("Generate() PASSED.")
+		}
+	}
+}
