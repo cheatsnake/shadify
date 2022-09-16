3aff3cd450bea40b587dd020205775c3a0e19f21
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2022-09-16 20:42:15 +0300

    test: Updated tests for Countries module

--- pkg/countries/core_test.go
+++ pkg/countries/core_test.go
@@ -13,31 +13,51 @@ func init() {
 }
 
 func TestGetCapitalQuiz(t *testing.T) {
-	validValues := []int{2, 4, 6}
-	for _, vv := range validValues {
+	validVariants := []int{2, 4, 6}
+	for _, vv := range validVariants {
 		result, _ := GetCapitalQuiz(vv, 1)
 		if len(result[0].Variants) != vv {
-			t.Errorf("GetCapitalQuiz(%d) FAILED. Expected result.Variants with length = %d, but got %d",
+			t.Errorf("GetCapitalQuiz(%d, 1) FAILED. Expected result.Variants with length = %d, but got %d",
 				vv, vv, len(result[0].Variants))
 		} else {
-			t.Logf("GetCapitalQuiz(%d) PASSED.", vv)
+			t.Logf("GetCapitalQuiz(%d, 1) PASSED.", vv)
 		}
 	}
 
-	invalidValues := []int{-2, 0, 1, 7, 10}
-	for _, iv := range invalidValues {
+	invalidVariants := []int{-2, 0, 1, 7, 10}
+	for _, iv := range invalidVariants {
 		_, err := GetCapitalQuiz(iv, 1)
 		if err == nil {
-			t.Errorf("GetCapitalQuiz(%d) FAILED. Expected error", iv)
+			t.Errorf("GetCapitalQuiz(%d, 1) FAILED. Expected an error", iv)
 		} else {
-			t.Logf("GetCapitalQuiz(%d) -> %s PASSED.", iv, err.Error())
+			t.Logf("GetCapitalQuiz(%d, 1) PASSED.", iv)
+		}
+	}
+
+	validAmounts := []int{0, 1, 5, 10, 20}
+	for _, va := range validAmounts {
+		result, _ := GetCapitalQuiz(2, va)
+		if len(result) != va {
+			t.Errorf("GetCapitalQuiz(2, %d) FAILED. Expected result with length %d, but got %d", va, va, len(result))
+		} else {
+			t.Logf("GetCapitalQuiz(2, %d) PASSED.", va)
+		}
+	}
+
+	invalidAmounts := []int{-1, -5, 21, 25}
+	for _, ia := range invalidAmounts {
+		_, err := GetCapitalQuiz(2, ia)
+		if err == nil {
+			t.Errorf("GetCapitalQuiz(2, %d) FAILED. Expected an error", ia)
+		} else {
+			t.Logf("GetCapitalQuiz(2, %d) PASSED.", ia)
 		}
 	}
 }
 
 func TestGetCountryQuiz(t *testing.T) {
-	validValues := []int{2, 4, 6}
-	for _, vv := range validValues {
+	validVariants := []int{2, 4, 6}
+	for _, vv := range validVariants {
 		result, _ := GetCountryQuiz(vv, 1)
 		if len(result[0].Variants) != vv {
 			t.Errorf("GetCountryQuiz(%d) FAILED. Expected result.Variants with length = %d, but got %d",
@@ -47,13 +67,33 @@ func TestGetCountryQuiz(t *testing.T) {
 		}
 	}
 
-	invalidValues := []int{-2, 0, 1, 7, 10}
-	for _, iv := range invalidValues {
+	invalidVariants := []int{-2, 0, 1, 7, 10}
+	for _, iv := range invalidVariants {
 		_, err := GetCountryQuiz(iv, 1)
 		if err == nil {
-			t.Errorf("GetCountryQuiz(%d) FAILED. Expected error", iv)
+			t.Errorf("GetCountryQuiz(%d) FAILED. Expected an error", iv)
 		} else {
-			t.Logf("GetCountryQuiz(%d) -> %s PASSED.", iv, err.Error())
+			t.Logf("GetCountryQuiz(%d) PASSED.", iv)
+		}
+	}
+
+	validAmounts := []int{0, 1, 5, 10, 20}
+	for _, va := range validAmounts {
+		result, _ := GetCountryQuiz(2, va)
+		if len(result) != va {
+			t.Errorf("GetCountryQuiz(2, %d) FAILED. Expected result with length %d, but got %d", va, va, len(result))
+		} else {
+			t.Logf("GetCountryQuiz(2, %d) PASSED.", va)
+		}
+	}
+
+	invalidAmounts := []int{-1, -5, 21, 25}
+	for _, ia := range invalidAmounts {
+		_, err := GetCountryQuiz(2, ia)
+		if err == nil {
+			t.Errorf("GetCountryQuiz(2, %d) FAILED. Expected an error", ia)
+		} else {
+			t.Logf("GetCountryQuiz(2, %d) PASSED.", ia)
 		}
 	}
 }
