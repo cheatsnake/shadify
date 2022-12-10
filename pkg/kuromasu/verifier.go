74acc4006e334c982d6d9f9c63ccbdff415312c4
cheatsnake <72691412+cheatsnake@users.noreply.github.com>
2022-12-10 10:13:10 +0300

    feat: Added verifier for Kuromasu module

---
+++ pkg/kuromasu/verifier.go
@@ -0,0 +1,31 @@
+package kuromasu
+
+func verifier(sol [][]string) VerifyResult {
+	test := make([][]string, len(sol))
+	
+	for i := range test {
+		row := make([]string, len(sol[0]))
+		copy(row, sol[i])
+		test[i] = row
+	} 
+
+	for i := range test {
+		for j := range test[i] {
+			if test[i][j] != boxCell {
+				test[i][j] = calcViewCount(j, i, &test)
+			}
+
+			if (sol[i][j] != emptyCell) {
+				isEqual := sol[i][j] == test[i][j]
+				
+				if !isEqual {
+					return VerifyResult{
+						IsValid: false, 
+						Position: position{Row: i+1, Column: j+1}}
+				}
+			}
+		}
+	}
+
+	return VerifyResult{IsValid: true}
+}
